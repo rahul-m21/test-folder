d04e046036e22c6bf1e924c3d7a8ed2b08c7bdd7
Rahul M <rahul.m@ptplavx024.payoda.net>
2025-01-28 17:09:23 +0530

    test

--- 2344.py
+++ 2344.py
@@ -9,7 +9,7 @@ public_key = key.publickey().export_key()
 
 # Save keys to files
 with open('private.pem', 'wb') as f:
-    f.write(private_key)
+    f.write(private_key)    
 with open('public.pem', 'wb') as f:
     f.write(public_key)
 
